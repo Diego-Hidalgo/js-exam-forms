b19a8cd7593657146009f8c59a7edd574d3081dd
Diego H <d13g01hl@gmail.com>
2022-04-26 17:21:50 -0500

    Renamed dynamic routes

---
+++ pages/student/[studentId]/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+export default function create() {
+    return (
+        <div>
+            <Head>
+
+            </Head>
+            <main>
+                <h1>It's possible to get to student/[id]!!!</h1>
+            </main>
+        </div>
+    )
+}
