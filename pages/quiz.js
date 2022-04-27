130f81a84aec5c02c4d3f7d547156eb598b0530a
BrianR <briomero76@gmail.com>
2022-04-27 07:34:41 -0500

    quizform added

--- pages/quiz.js
+++ pages/quiz.js
@@ -2,5 +2,19 @@ import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function quizForm(){
-    
+
+    let addQuestion = () => {
+        alert("You have been registered")
+        var question = document.getElementById("questionSection");
+        var div = document.createElement("div");
+        div.innerHTML = ""
+        question.appendChild(div);
+      }
+
+    return (
+        <div>
+            <div id = "questionSection"></div>
+            <button onclick = {addQuestion}>Add question</button>
+        </div>
+    )
 }
