4629d2d0bf4fd4cbcca835e31bf43ac1071c15ba
Diego H <d13g01hl@gmail.com>
2022-04-27 14:16:01 -0500

    Added provisional design for the student index page

--- pages/student/[studentId]/index.js
+++ pages/student/[studentId]/index.js
@@ -2,15 +2,68 @@ import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
-export default function create() {
-    return (
-        <div>
-            <Head>
+export default function create() {  
 
-            </Head>
-            <main>
-                <h1>It's possible to get to student/[id]!!!</h1>
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+
+      </Head>
+      <main>
+        <section class="vh-100" style={{backgroundColor: '#2779e2'}}>
+          <div class="d-flex flex-row justify-content-center align-items-center">
+            <h1 class="text-white mb-4">Welcome</h1>
+          </div>
+          <div id="addHere" class="d-flex flex-column align-items-center">
+            <div class="d-flex flex-row">
+              <div class="mx-10 card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
+                <div class="card-body">
+                  <h5 class="card-title">Exam title</h5>
+                  <p class="card-text">Provide a brief optional description for the exam</p>
+                  <a href="" class="btn btn-primary">Application Page</a>
+                </div>
+              </div>
+              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
+                <div class="card-body">
+                  <h5 class="card-title">Exam title</h5>
+                  <p class="card-text">Provide a brief optional description for the exam</p>
+                  <a href="" class="btn btn-primary">Application Page</a>
+                </div>
+              </div>
+              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
+                <div class="card-body">
+                  <h5 class="card-title">Exam title</h5>
+                  <p class="card-text">Provide a brief optional description for the exam</p>
+                  <a href="" class="btn btn-primary">Application Page</a>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex flex-row">
+              <div class="mx-10 card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
+                <div class="card-body">
+                  <h5 class="card-title">Exam title</h5>
+                  <p class="card-text">Provide a brief optional description for the exam</p>
+                  <a href="" class="btn btn-primary">Application Page</a>
+                </div>
+              </div>
+              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
+                <div class="card-body">
+                  <h5 class="card-title">Exam title</h5>
+                  <p class="card-text">Provide a brief optional description for the exam</p>
+                  <a href="" class="btn btn-primary">Application Page</a>
+                </div>
+              </div>
+              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
+                <div class="card-body">
+                  <h5 class="card-title">Exam title</h5>
+                  <p class="card-text">Provide a brief optional description for the exam</p>
+                  <a href="" class="btn btn-primary">Application Page</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  )
 }
