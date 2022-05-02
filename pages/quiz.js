16494d361f5fc8f3088922e6f197d7a612986a0c
BrianR <briomero76@gmail.com>
2022-05-02 05:19:38 -0500

    minor changes on questions form

--- pages/quiz.js
+++ pages/quiz.js
@@ -2,39 +2,67 @@ import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 import { makePublicRouterInstance } from 'next/router'
 
+let question = "1";
+
 export default function quizForm(){
+  console.log(question)
   let addQuestion = () => {
-      console.log("Agregando formulario...")
-      var question = document.getElementById("questionSection");
-      var div = document.createElement("div");
-      div.innerHTML =
-        "<div class='d-flex justify-content-center'>"+
-          "<input type='text' placeholder='Ingrese aqui la pregunta'></div>"+
-          "<div id = 'first-row'>"
-              "<div id = 'first_question'>"+
-                "<input type='radio' name = 'answer'>"+
-                "<input type='text'>"+
-              "</div>"+
-              "<div id = 'Second_question'>"+
-                "<input type='radio' name = 'answer'>"+
-                "<input type='text'>"+
-          "</div></div>"+
+    console.log("Agregando formulario...")
+    var question = document.getElementById("questionSection");
+    var div = document.createElement("div");
+    div.innerHTML =
+      "<div class='d-flex justify-content-center'>"+
+      "<div class='d-flex flex-column'>"+
+        "<input type='text' placeholder='Ingrese aqui la pregunta' size = 100>"+
+        "<div class='h-50 d-inline-block'></br></div>"+
+        "<div class='d-flex align-items-center' id = 'first-row'>"+
+          "<div id = 'first_question'>"+
+            "<input type='radio' name = 'answer' id = \'"+question+"-1\'>"+
+            "<input type='text' placeholder='Ingrese respuesta 1' size = 45>"+
+          "</div>"+
+          "<div id = 'Second_question'>"+
+            "<input type='radio' name = 'answer'>"+
+            "<input type='text' placeholder='Ingrese respuesta 2' size = 45>"+
+          "</div>"+
+      
+        "</div>"+
+        "<div class='w-50 p-3' id = 'middle-row'></div>"+
+        "<div class='d-flex align-items-center' id = 'second-row'>"+
           "<div id = 'Third_question'>"+
-          "<input type='radio' name = 'answer'>"+
-          "<input type='text'>"+
+            "<input type='radio' name = 'answer'>"+
+            "<input type='text' placeholder='Ingrese respuesta 3' size = 45>"+
           "</div>"+
           "<div id = 'Fourth_question'>"+
-          "<input type='radio' name = 'answer'>"+
-          "<input type='text'>"+
-          "</div>"
+            "<input type='radio' name = 'answer'>"+
+            "<input type='text' placeholder='Ingrese respuesta 4' size = 45>"+
+          "</div>"+
+        "</div>"+
+        "</div>"+
+      "</div>"+
+      "<div class='w-50 p-3' id = 'paddle_afterQuestions'></div>"
       question.appendChild(div);
-     // document.getElementById("hellobutton").addEventListener("addQuestionBtn", addQuestion);
+      question++;
   }
 
   return (
       <div>
-          <div id = "questionSection"></div>
-          <button type = "button" onMouseDown= {addQuestion} >Add question</button>
+        <section>
+         <nav class="navbar navbar-expand-lg d-flex justify-content-center align-items-center">
+            <a class="navbar-brand" href="/">
+              <h5>js-exam-forms</h5>
+            </a>
+          </nav>
+        </section>
+        <section class="vh-100" style={{backgroundColor: '#2779e2'}}>
+        <div class='w-100 p-3' id = "top-padding">
+          </div>
+          <div id = "questionSection" style={{backgroundColor: '#2779e2'}}></div>
+          <div class='d-flex justify-content-center' id = "addQuestion" style={{backgroundColor: '#2779e2'}}>
+            <button type = "button" onMouseDown= {addQuestion} class="btn btn-warning">Add question</button>
+            <div class='w-15 p-3' id = 'paddle_afterQuestions'></div>
+            <button type = "button" class="btn btn-success">Add question</button>
+          </div>
+        </section>
       </div>
   )
 }
