7d919c6dc44700511d8cee20314d974728206e7a
BrianR <briomero76@gmail.com>
2022-05-02 12:05:54 -0500

    check fills added

--- pages/quiz.js
+++ pages/quiz.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 import { makePublicRouterInstance } from 'next/router'
 
-let quest = 1;
+let quest = 0;
 export default function quizForm(){
   let addQuestion = () => {
     console.log("Agregando formulario...")
@@ -13,38 +13,38 @@ export default function quizForm(){
       "<div class='d-flex flex-column'>"+
       "<div class='input-group mb-3'>"+
         "<div class='input-group-prepend'>"+
-        "<span class='input-group-text' id='basic-addon1'>"+quest+"</span>"+
+        "<span class='input-group-text' id='basic-addon1'>"+(quest+1)+"</span>"+
         "</div>"+
-        "<input type='text' placeholder='Ingrese aqui la pregunta' size = 100 id = Q-\'"+quest+"-1\' >"+
+        "<input type='text' placeholder='Ingrese aqui la pregunta' size = 100 id =\'Q-"+(quest+1)+"\' >"+
       "</div>"+
         "<div class='h-50 d-inline-block'></br></div>"+
         "<div class='d-flex align-items-center' id = 'first-row'>"+
           "<div class='input-group mb-3' id = 'first_question'>"+
             "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-a\' checked>"+
+            "<input type='radio' name = 'answer' id = \'"+(quest+1)+"-a\' checked>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 1' size = 45 id = \'"+quest+"-1\'>"+
+            "<input type='text' placeholder='Ingrese respuesta 1' size = 45 id = \'"+(quest+1)+"-1\'>"+
           "</div>"+
           "<div class='input-group mb-3' id = 'Second_question'>"+
           "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-b\'>"+
+            "<input type='radio' name = 'answer' id = \'"+(quest+1)+"-b\'>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 2' size = 45 id = \'"+quest+"-2\'>"+
+            "<input type='text' placeholder='Ingrese respuesta 2' size = 45 id = \'"+(quest+1)+"-2\'>"+
           "</div>"+
         "</div>"+
         "<div class='w-50 p-3' id = 'middle-row'></div>"+
         "<div class='d-flex align-items-center' id = 'second-row'>"+
           "<div class='input-group mb-3' id = 'Third_question'>"+
           "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-c\'>"+
+            "<input type='radio' name = 'answer' id = \'"+(quest+1)+"-c\'>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 3' size = 45 id = \'"+quest+"-3\'>"+
+            "<input type='text' placeholder='Ingrese respuesta 3' size = 45 id = \'"+(quest+1)+"-3\'>"+
           "</div>"+
           "<div class='input-group mb-3' id = 'Fourth_question'>"+
           "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-d\'>"+
+            "<input type='radio' name = 'answer' id = \'"+(quest+1)+"-d\'>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 4' size = 45 id = \'"+quest+"-4\'>"+
+            "<input type='text' placeholder='Ingrese respuesta 4' size = 45 id = \'"+(quest+1)+"-4\'>"+
           "</div>"+
         "</div>"+
         "</div>"+
@@ -54,26 +54,30 @@ export default function quizForm(){
       quest += 1;
   }
 
-  let sendQuestions = () =>{
-    if(checkFields)
-      alert(".value")
-  }
+  let sendQuestions = () => {
+    if(quest >0){
+      if(checkFields()){
+        console.log(checkFields())
+      }else {
+        alert("Fill all field before to send the questions")}
+    }else{alert("Add a question first")}//End if..else
+  }//End sendQuestion
 
-  let checkFields = () => {
-    var check = false;
-    var letter = ['a','b','c','d']
-    for(let i = 1; i <= quest && !check; i++){
+  function checkFields() {
+    var fill = true;
+    for(let i = 1; i <= quest && fill; i++){
       var q_head = document.getElementById("Q-"+i);
       var q1 = document.getElementById(i+"-1");
       var q2 = document.getElementById(i+"-2");
       var q3 = document.getElementById(i+"-3");
       var q4 = document.getElementById(i+"-4");
-      check = (q_head == "")?true:false;
-      check = (q2 == "" && !check)?true:false;
-      check = (q3 == "" && !check)?true:false;
-      check = (q4 == "" && !check)?true:false;
+      fill = (q_head.value == "")?false:true;
+      fill = (q1.value == "" && fill)?false:true;
+      fill = (q2.value == "" && fill)?false:true;
+      fill = (q3.value == "" && fill)?false:true;
+      fill = (q4.value == "" && fill)?false:true;
       }//End for
-      return check;
+      return fill;
     }//End checkFields
 
   return (
