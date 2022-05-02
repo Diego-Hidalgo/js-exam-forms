bcdc013a3a579daae739a992b98ba6be98d96577
BrianR <briomero76@gmail.com>
2022-05-02 10:55:08 -0500

    unos errores tee~hee

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 import { makePublicRouterInstance } from 'next/router'
+import { Alert } from 'bootstrap';
 
-let quest = 1;
+let quest = 0;
 
 export default function quizForm(){
   let addQuestion = () => {
+    quest++;
     console.log("Agregando formulario...")
     var question = document.getElementById("questionSection");
     var div = document.createElement("div");
@@ -13,36 +15,39 @@ export default function quizForm(){
       "<div class='d-flex justify-content-center'>"+
       "<div class='d-flex flex-column'>"+
       "<div class='input-group mb-3'>"+
-        "<input type='text' placeholder='Ingrese aqui la pregunta' size = 100>"+
+        "<div class='input-group-prepend'>"+
+        "<span class='input-group-text' id='basic-addon1'>"+quest+"</span>"+
+        "</div>"+
+        "<input type='text' placeholder='Ingrese aqui la pregunta' size = 100 id = Q-\'"+quest+"\' >"+
       "</div>"+
         "<div class='h-50 d-inline-block'></br></div>"+
         "<div class='d-flex align-items-center' id = 'first-row'>"+
           "<div class='input-group mb-3' id = 'first_question'>"+
             "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-1\'>"+
+            "<input type='radio' name = 'answer' id = \'"+quest+"-a\' checked>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 1' size = 45>"+
+            "<input type='text' placeholder='Ingrese respuesta 1' size = 45 id = \'"+quest+"-1\'>"+
           "</div>"+
           "<div class='input-group mb-3' id = 'Second_question'>"+
           "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-2\'>"+
+            "<input type='radio' name = 'answer' id = \'"+quest+"-b\'>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 2' size = 45>"+
+            "<input type='text' placeholder='Ingrese respuesta 2' size = 45 id = \'"+quest+"-2\'>"+
           "</div>"+
         "</div>"+
         "<div class='w-50 p-3' id = 'middle-row'></div>"+
         "<div class='d-flex align-items-center' id = 'second-row'>"+
           "<div class='input-group mb-3' id = 'Third_question'>"+
           "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-3\'>"+
+            "<input type='radio' name = 'answer' id = \'"+quest+"-c\'>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 3' size = 45>"+
+            "<input type='text' placeholder='Ingrese respuesta 3' size = 45 id = \'"+quest+"-3\'>"+
           "</div>"+
           "<div class='input-group mb-3' id = 'Fourth_question'>"+
           "<div class='input-group-prepend'>"+
-            "<input type='radio' name = 'answer' id = \'"+quest+"-4\'>"+
+            "<input type='radio' name = 'answer' id = \'"+quest+"-d\'>"+
             "</div>"+
-            "<input type='text' placeholder='Ingrese respuesta 4' size = 45>"+
+            "<input type='text' placeholder='Ingrese respuesta 4' size = 45 id = \'"+quest+"-4\'>"+
           "</div>"+
         "</div>"+
         "</div>"+
@@ -51,9 +56,11 @@ export default function quizForm(){
       question.appendChild(div);
       quest += 1;
   }
-
   let sendQuestions = () =>{
-    alert(quest)
+   /* if(quest > 0){
+      if(true)
+      alert("Fill all fields before to send the questions")
+    }else Alert("You haven't made questions")*/
   }
 
   return (
