dbf95c6b11a352a57943295be1a2ca359d50cbc5
Diego H <d13g01hl@gmail.com>
2022-05-07 20:01:21 -0500

    Changed Students exam view design

--- pages/student/[studentId]/index.js
+++ pages/student/[studentId]/index.js
@@ -1,82 +1,52 @@
-import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
-import { redirectToExam } from '/pages/utils/manageExams'
+import { logout } from '../../utils/logout';
 
-export default function create() {  
-  
+export default function create() {
+
   const QUEST_PATH = '/api/exams'
 
-  let algo = async () =>{
+  let loadExams = async () => {
+
+    let url;
+    if (typeof window !== "undefined") {
+      url = window.location.href
+    }
+    let url_pieces = url.split('/')
+    
+    let addIn = document.getElementById('addHere')
+    let div = document.createElement('div')
+
     let res = await fetch(QUEST_PATH)
+
     let js = await res.json()
-    redirectToExam(js[0])
+    for(let i = 0; i < js.length; i ++) {
+      div.innerHTML += "<div class='d-flex flex-row>'>" +
+      "<div class='mx-10 card card-container' style='margin: 10px'>"+
+        "<div class='card-body'>" +
+          "<h5 class='card-title'> EXAM #" + (i+1) + "</h5>" +
+          "<p class='card-text'>" + js[i].id + "</p>"+
+          "<a href='"+url_pieces[4]+"/exam/"+ js[i].id +"' class='btn btn-primary'> Take Exam </a>" +
+        "</div>"+
+      "</div>"    
+    }
+    addIn.appendChild(div)
   }
 
   return (
     <div>
-      <Head>
-
-      </Head>
       <main>
         <section>
           <nav class="navbar navbar-expand-lg d-flex justify-content-center align-items-center">
-            <button class="btn btn-danger">
+            <button class="btn btn-danger" onMouseDown={logout}>
               LOG OUT
             </button>
           </nav>
         </section>
-        <section class="vh-100" style={{backgroundColor: '#2779e2'}}>
+        <section class="vh" style={{backgroundColor: '#2779e2'}} onLoad={loadExams()}>
           <div class="d-flex flex-row justify-content-center align-items-center">
             <h1 class="text-white mb-4">Welcome</h1>
           </div>
-          <div id="addHere" class="d-flex flex-column align-items-center">
-            <div class="d-flex flex-row">
-              <div class="mx-10 card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
-                <div class="card-body">
-                  <h5 class="card-title">Exam title</h5>
-                  <p class="card-text">Provide a brief optional description for the exam</p>
-                  <a href="" class="btn btn-primary">Application Page</a>
-                </div>
-              </div>
-              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
-                <div class="card-body">
-                  <h5 class="card-title">Exam title</h5>
-                  <p class="card-text">Provide a brief optional description for the exam</p>
-                  <a href="" class="btn btn-primary">Application Page</a>
-                </div>
-              </div>
-              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
-                <div class="card-body">
-                  <h5 class="card-title">Exam title</h5>
-                  <p class="card-text">Provide a brief optional description for the exam</p>
-                  <a href="" class="btn btn-primary">Application Page</a>
-                </div>
-              </div>
-            </div>
-            <div class="d-flex flex-row">
-              <div class="mx-10 card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
-                <div class="card-body">
-                  <h5 class="card-title">Exam title</h5>
-                  <p class="card-text">Provide a brief optional description for the exam</p>
-                  <a href="" class="btn btn-primary">Application Page</a>
-                </div>
-              </div>
-              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
-                <div class="card-body">
-                  <h5 class="card-title">Exam title</h5>
-                  <p class="card-text">Provide a brief optional description for the exam</p>
-                  <a href="" class="btn btn-primary">Application Page</a>
-                </div>
-              </div>
-              <div class="card card-container" style={{width: 18 + 'rem', padding: 20 + 'px', margin: 10}}>
-                <div class="card-body">
-                  <h5 class="card-title">Exam title</h5>
-                  <p class="card-text">Provide a brief optional description for the exam</p>
-                  <a href="" class="btn btn-primary">Application Page</a>
-                </div>
-              </div>
-            </div>
-            <button onMouseDown={algo}>Yeah</button>
+          <div id="addHere" class="d-flex flex-column align-items-center"> 
           </div>
         </section>
       </main>
